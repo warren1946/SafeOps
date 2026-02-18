e1bdd2f4f55ad8942ff079019ce50f7b6778a346
WHMTAWU <warrenmtawu@gmail.com>
2026-02-18 13:53:20 +0200

    - This module consist(s) of: -- Creation of a Hazard -- Listing of a Hazards -- Fetching Hazard from db -- Update Hazard details -- Resolving of a Hazard -- Assign a Hazard to an individual

---
+++ src/main/kotlin/com/zama/safeops/modules/hazards/infrastructure/persistence/entities/HazardJpaEntity.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026 WH Mtawu.
+ * Open Source under the MIT License.
+ * Permission granted for use, modification, and distribution with attribution.
+ * No warranty provided.
+ */
+
+package com.zama.safeops.modules.hazards.infrastructure.persistence.entities
+
+import com.zama.safeops.modules.hazards.domain.model.HazardStatus
+import jakarta.persistence.*
+import java.time.Instant
+
+@Entity
+@Table(name = "hazards")
+class HazardJpaEntity(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    val title: String,
+    val description: String,
+
+    @Enumerated(EnumType.STRING)
+    val status: HazardStatus,
+
+    val assignedTo: Long? = null,
+
+    val createdAt: Instant,
+    val updatedAt: Instant
+)
