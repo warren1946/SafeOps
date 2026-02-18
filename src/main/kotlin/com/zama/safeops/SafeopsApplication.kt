048051d6bb1cfeda976e57b34a347d611dd67269
WHMTAWU <warrenmtawu@gmail.com>
2026-02-18 14:42:43 +0200

    - This module consist(s) of: -- fixing an error

--- src/main/kotlin/com/zama/safeops/SafeopsApplication.kt
+++ src/main/kotlin/com/zama/safeops/SafeopsApplication.kt
@@ -8,11 +8,15 @@
 package com.zama.safeops
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.persistence.autoconfigure.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EntityScan("com.zama.safeops")
+@EnableJpaRepositories("com.zama.safeops")
 class SafeopsApplication
 
 fun main(args: Array<String>) {
-	runApplication<SafeopsApplication>(*args)
+    runApplication<SafeopsApplication>(*args)
 }
